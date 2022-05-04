40604e799ba4b80fbb254412d45c9620e236576a
Stephanie <stephaniiehallberg@gmail.com>
2022-05-04 09:19:36 +0200

    feat(ride): uber car options

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,10 +11,10 @@ const MapScreen = () => {
   const Stack = createNativeStackNavigator();
   return (
     <View>
-      <View style={tw`h-1/2`}>
+      <View style={tw`h-1/3`}>
         <Map />
       </View>
-      <View style={tw`h-1/2`}>
+      <View style={tw`h-2/3`}>
         <Stack.Navigator>
           <Stack.Screen
             name="NavigateCard"
